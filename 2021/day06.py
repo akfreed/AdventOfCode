937022a078192691d5bd885f18020efa0818c9f0
akfreed <alexkgray@gmail.com>
2021-12-05 21:51:40 -0800

    Day 6 cleanup.

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,59 +1,36 @@
 
-def get_input_a():
+def get_input():
     with open('day06.txt', 'r') as file:
         return [int(n) for n in file.readline().strip().split(',')]
 
 
-def get_input_b():
-    return get_input_a()
+def run_fish(fish_list, num_generations):
+    adult_fish = {i: sum(map(lambda n: 1 if n == i else 0, fish_list)) for i in range(7)}
+    baby_fish = {i: 0 for i in range(7)}
 
+    for i in range(num_generations):
+        day = i % 7
+        baby_fish[(day + 2) % 7] = adult_fish[day]
+        adult_fish[day] += baby_fish[day]
+        baby_fish[day] = 0
 
-class Fish:
-    def __init__(self, n=8):
-        self.timer = n
-
-    def update1(self):
-        self.timer -= 1
-
-    def update2(self):
-        if self.timer == -1:
-            self.timer = 6
+    return sum(adult_fish.values()) + sum(baby_fish.values())
 
 
 def day6a():
     print("    Part A")
-    fish = get_input_a()
-    fish = [Fish(n) for n in fish]
-
-    for i in range(80):
-        to_add = []
-        for f in fish:
-            f.update1()
-            if f.timer == -1:
-                to_add.append(Fish())
-            f.update2()
-        fish = fish + to_add
-    print(len(fish))
+    fish_list = get_input()
+    num = run_fish(fish_list, 80)
+    print(num)
+    assert num == 352872
 
 
 def day6b():
     print("\n    Part B")
-    fish = get_input_b()
-    fish_map = {}
-    fish2 = {}
-    for n in range(7):
-        fish_map[n] = 0
-        fish2[n] = 0
-    for n in fish:
-        fish_map[n] += 1
-
-    for i in range(258):
-        day = i % 7
-        fish2[(day + 2) % 7] = fish_map[day]
-        fish_map[day] += fish2[day]
-        fish2[day] = 0
-
-    print(sum(fish_map.values()))
+    fish_list = get_input()
+    num = run_fish(fish_list, 256)
+    print(num)
+    assert num == 1604361182149
 
 
 if __name__ == '__main__':

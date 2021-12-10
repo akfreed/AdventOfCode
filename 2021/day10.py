cdbc0bca00987839adf75c605039c5a068448701
akfreed <alexkgray@gmail.com>
2021-12-09 21:51:05 -0800

    Day 10 cleanup.

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,97 +1,68 @@
-def get_input_a():
+def get_input():
     with open('day10.txt', 'r') as file:
         return [line.strip() for line in file.readlines()]
 
 
-def get_input_b():
-    return get_input_a()
-
-
-def check_line(line):
+def score_line(line):
+    """Return the score as a negative if the line is corrupted. Return a positive score if incomplete. 0 otherwise."""
     stack = []
     for char in line:
         if char == '(' or char == '{' or char == '[' or char == '<':
-            stack.insert(0, char)
-        elif char == ')':
-            if stack[0] != '(':
-                return 3
-            stack.pop(0)
-        elif char == ']':
-            if stack[0] != '[':
-                return 57
-            stack.pop(0)
-        elif char == '}':
-            if stack[0] != '{':
-                return 1197
-            stack.pop(0)
-        elif char == '>':
-            if stack[0] != '<':
-                return 25137
-            stack.pop(0)
-    #assert len(stack) == 0
-    return 0
+            stack.append(char)
+        else:
+            if char == ')':
+                if stack[-1] != '(':
+                    return -3
+            elif char == ']':
+                if stack[-1] != '[':
+                    return -57
+            elif char == '}':
+                if stack[-1] != '{':
+                    return -1197
+            elif char == '>':
+                if stack[-1] != '<':
+                    return -25137
+            else:
+                assert False
+            stack.pop()
 
-
-def check_line2(line):
-    stack = []
-    for char in line:
-        if char == '(' or char == '{' or char == '[' or char == '<':
-            stack.insert(0, char)
-        elif char == ')':
-            if stack[0] != '(':
-                return 0
-            stack.pop(0)
-        elif char == ']':
-            if stack[0] != '[':
-                return 0
-            stack.pop(0)
-        elif char == '}':
-            if stack[0] != '{':
-                return 0
-            stack.pop(0)
-        elif char == '>':
-            if stack[0] != '<':
-                return 0
-            stack.pop(0)
-
-    pts = 0
-    ret = ''
-    for char in stack:
-        pts *= 5
+    points = 0
+    for char in reversed(stack):
+        points *= 5
         if char == '(':
-            pts += 1
-            ret += ')'
-        if char == '[':
-            pts += 2
-            ret += ']'
-        if char == '{':
-            pts += 3
-            ret += '}'
-        if char == '<':
-            pts += 4
-            ret += '}'
-
-    return pts
+            points += 1
+        elif char == '[':
+            points += 2
+        elif char == '{':
+            points += 3
+        elif char == '<':
+            points += 4
+        else:
+            assert False
+    return points
 
 
 def day10a():
     print("    Part A")
-    lines = get_input_a()
-    agg = 0
-    for line in lines:
-        agg += check_line(line)
-    print(agg)
+    lines = get_input()
+    scores = [score_line(line) for line in lines]
+    score = sum([-score for score in scores if score < 0])
+    print(score)
+    assert score == 288291
+    return score
 
 
 def day10b():
     print("\n    Part B")
-    lines = get_input_b()
-    #lines = [line for line in lines if check_line(line) == 0]
-    scores = sorted([check_line2(line) for line in lines if check_line2(line) != 0])
-    print(scores[len(scores)//2])
-
+    lines = get_input()
+    scores = [score_line(line) for line in lines]
+    scores = sorted([score for score in scores if score > 0])
+    median = scores[len(scores) // 2]
+    print(median)
+    assert median == 820045242
+    return median
 
 
 if __name__ == '__main__':
-    #day10a()
+    day10a()
     day10b()

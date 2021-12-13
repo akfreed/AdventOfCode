01eb05fa6f3e78489c3ce50f4313c812e639e10c
akfreed <alexkgray@gmail.com>
2021-12-12 21:50:09 -0800

    Day 13 cleanup.

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,69 +1,67 @@
-from utility import *
+from utility import Vector, render_map
 
 
-def get_input_a():
+def get_input():
     with open('day13.txt', 'r') as file:
         lines = [line.strip() for line in file.readlines()]
 
+    # Read dot coordinates.
     coords = []
-    z = 0
+    fold_instructions_idx = 0
     for i, line in enumerate(lines):
         if line == "":
-            z = i
+            fold_instructions_idx = i + 1
             break
         x, y = line.split(",")
         coords.append(Vector(int(x), int(y)))
-    folds = [line.split("fold along ")[1] for line in lines[z+1:]]
+
+    folds = [line.split("fold along ")[1] for line in lines[fold_instructions_idx:]]
     folds = [fold.split('=') for fold in folds]
-    folds = [(fold[0], int(fold[1])) for fold in folds]
+    folds = [(axis, int(line)) for axis, line in folds]
     return coords, folds
 
 
-def get_input_b():
-    return get_input_a()
-
-
 def fold_once(dots, fold):
-    if fold[0] == 'x':
+    axis, line_num = fold
+    if axis == 'x':
         for dot in dots:
-            if dot.x > fold[1]:
-                dot.x -= (dot.x - fold[1]) * 2
-    elif fold[0] == 'y':
+            if dot.x > line_num:
+                dot.x -= (dot.x - line_num) * 2
+    elif axis == 'y':
         for dot in dots:
-            if dot.y > fold[1]:
-                dot.y -= (dot.y - fold[1]) * 2
+            if dot.y > line_num:
+                dot.y -= (dot.y - line_num) * 2
     else:
         assert False
 
 
-def count_dots(dots):
-    print(len(dots))
-    s = {}
+def deduplicate(dots):
+    deduped = {}
     for dot in dots:
-        if dot not in s:
-            s[dot] = '##'
-
-    return s
+        if dot not in deduped:
+            # Add dot to the map and set the value to ## (for printing later)
+            deduped[dot] = '##'
+    return deduped
 
 
 def day13a():
     print("    Part A")
-    dots, folds = get_input_a()
+    dots, folds = get_input()
     fold_once(dots, folds[0])
-    s = count_dots(dots)
-    print(len(s))
-    # not 748
+    dots = deduplicate(dots)
+    print(len(dots))
+    assert len(dots) == 759
+    return len(dots)
 
 
 def day13b():
     print("\n    Part B")
-    dots, folds = get_input_a()
+    dots, folds = get_input()
     for fold in folds:
         fold_once(dots, fold)
-
-    s = count_dots(dots)
-    render_map(s, str, '..')
-    pass
+        
+    dots = deduplicate(dots)
+    render_map(dots, str, '  ')
 
 
 if __name__ == '__main__':
